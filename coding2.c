10ffa6b3fcc7306e432a83e8db7ee6931651eecb
Fatricius <99702354+Fatricius@users.noreply.github.com>
2022-02-15 11:24:15 +0700

    Create coding2.c

---
+++ coding2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int iterativeBinarySearch(int array[], int start_index, int end_index, int element){
+   while (start_index <= end_index){
+      int middle = start_index + (end_index- start_index )/2;
+      if (array[middle] == element)
+         return middle;
+      if (array[middle] < element)
+         start_index = middle + 1;
+      else
+         end_index = middle - 1;
+   }
+   return -1;
+}
